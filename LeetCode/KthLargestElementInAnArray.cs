760e0d8af3d61dc1fb1f07c30f3eaa689a5eb533
Ryan Afshar <ryanafsharryan@gmail.com>
2025-06-16 21:20:45 -0700

    Add KthLargestElementInAnArray

---
+++ LeetCode/KthLargestElementInAnArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+
+        foreach(int num in nums)
+        {
+            pq.Enqueue(num, num);
+        }
+        int curr = 0;
+        for(int i =0; i< k;i++)
+        {
+            curr = pq.Dequeue();
+            //Console.WriteLine($"num {curr}");
+        }
+        return curr;
+    }
+}
